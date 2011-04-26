32137efce7609f1c03d683b96a18eca27bff2e52
Pieter Degroote <pieter.degroote@ster.kuleuven.be>
2011-04-26 08:37:17 +0200

    filters.py/get_response returns now ordered arrays

--- sed/filters.py
+++ sed/filters.py
@@ -45,7 +45,8 @@ def get_response(photband):
     """
     photfile = os.path.join(basedir,'filters',photband.upper())
     wave, response = ascii.read2array(photfile).T[:2]
-    return wave,response
+    sa = np.argsort(wave)
+    return wave[sa],response[sa]
 
 
 
@@ -65,11 +66,11 @@ def list_response(name='*',wave_range=(-np.inf,+np.inf)):
     @return: list of curve files
     @rtype: list of str
     """
-    #-- collect all curve files
-    curve_files = sorted(glob.glob(os.path.join(basedir,glob_string.upper())))
+    #-- collect all curve files but remove human eye responses
+    curve_files = sorted(glob.glob(os.path.join(basedir,'filters',name.upper())))
+    curve_files = [cf for cf in curve_files if not ('HUMAN' in cf or 'EYE' in cf) ]
     #-- select in correct wavelength range
-    curve_files = [os.path.basename(curve_file) for curve_file in curvefiles \
-      if (wave_range[0]<=effwave(curve_files)<=wave_range[1])]
+    curve_files = [os.path.basename(curve_file) for curve_file in curve_files if (wave_range[0]<=eff_wave(os.path.basename(curve_file))<=wave_range[1])]
     #-- log to the screen and return
     for curve_file in curve_files: logger.info(curve_file)
     return curve_files
@@ -113,21 +114,45 @@ def eff_wave(photband):
             
     return my_eff_wave
 
-
 @memoized
 def get_info(photbands=None):
     """
     Return a record array containing all filter information.
+    
+    The record arrays contains following columns:
+        - photband
+        - eff_wave
+        - type
+        - vegamag, vegamag_lit
+        - ABmag, ABmag_lit
+        - STmag, STmag_lit
+        - Flam0, Flam0_units, Flam0_lit
+        - Fnu0, Fnu0_units, Fnu0_lit,
+        - source
+    
+    @param photbands: list of photbands to get the information from. The input
+    order is equal to the output order. If C{None}, all filters are returned.
+    @type photbands: iterable container (list, tuple, 1Darray)
+    @return: record array containing all information on the requested photbands.
+    @rtype: record array
     """
     zp_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),'zeropoints.dat')
     zp = ascii.read2recarray(zp_file)
     zp = zp[np.argsort(zp['photband'])]
     
+    #-- list photbands in order given, and remove those that do not have
+    #   zeropoints etc.
     if photbands is not None:
-        keep = np.searchsorted(zp['photband'],photbands)
+        order = np.searchsorted(zp['photband'],photbands)
+        zp = zp[order]
+        keep = (zp['photband']==photbands)
         zp = zp[keep]
     
     return zp
+
+
+
+
 
 def update_info(zp):
     """
