67d810f3c98c9b8d7be1d8f41e75b4e6863ed43a
Pieter Degroote <pieter.degroote@ster.kuleuven.be>
2011-05-02 15:53:14 +0200

    io/fits.py reads corot FITS files

--- io/fits.py
+++ io/fits.py
@@ -9,6 +9,7 @@ import pyfits
 
 import numpy as np
 from ivs.misc import loggers
+from ivs.units import conversions
 
 logger = logging.getLogger("IO.FITS")
 logger.addHandler(loggers.NullHandler)
@@ -43,5 +44,58 @@ def read_spectrum(filename, return_header=False):
         return wave,flux,header
     else:
         return wave,flux
+
+
+def read_corot(fits_file,  return_header=False, type_data='hel',
+                         channel='sismo', remove_flagged=True):
+    """
+    Read CoRoT data from a CoRoT FITS file.
     
+    Currently only SISMO data is implemented.
+    
+    type_data is one of:
+        - type_data='raw'
+        - type_data='hel': heliocentric unequidistant
+        - type_data='helreg': heliocentric equidistant
+    
+    @param fits_file: CoRoT FITS file name
+    @type fits_file: string
+    @param return_header: return header information as dictionary
+    @type return_header: bool
+    @param type_data: type of data to return
+    @type type_data: string (one of 'raw','hel' or 'helreg')
+    @param channel: type of CoRoT data
+    @type channel: string (one of 'sismo' or 'exo')
+    @param remove_flagged: remove flagged datapoints
+    @type remove_flagged: True
+    @return: CoRoT data (times, flux, error, flags)
+    @type: array, array, array, array(, header)
+    """
+    #-- read in the FITS file
+    # headers: ['DATE', 'DATEJD', 'DATEHEL', 'STATUS', 'WHITEFLUX', 'WHITEFLUXDEV', 'BG', 'CORREC']
+    fits_file_    = pyfits.open(fits_file)
+    times,flux,error,flags = fits_file_[type_data].data.field(0),\
+                             fits_file_[type_data].data.field(1),\
+                             fits_file_[type_data].data.field(2),\
+                             fits_file_[type_data].data.field(3)
+    # extract the header if asked
+    if return_header:
+        header = fits_file_[0].header
+    fits_file_.close()
+    
+    logger.debug('Read CoRoT file %s'%(fits_file))
+    
+    # remove flagged datapoints if asked
+    if remove_flagged:
+        keep = (flags==0) & (error!=-1)
+        times,flux,error,flags = times[keep], flux[keep], error[keep], flags[keep]
+    
+    # convert times to heliocentric JD
+    times = conversions.convert('MJD','JD',times,jtype='corot')
+    
+    if return_header:
+        return times, flux, error, flags, header
+    else:
+        return times, flux, error, flags
+
 #}
