51f78ff78ea31143326bd7265f460ac40869e5de
Pieter Degroote <pieter.degroote@ster.kuleuven.be>
2011-06-24 16:38:24 +0200

    rochepotential.test

--- binary/rochepotential.py
+++ binary/rochepotential.py
@@ -945,7 +945,7 @@ def surface_elements((r,mygrid),(surfnormal_x,surfnormal_y,surfnormal_z),gtype='
             
         #theta,phi = np.arccos(centers[:,2]),np.arctan2(centers[:,1],centers[:,0])+pi
         mlab.points3d(centers[:,0],centers[:,1],centers[:,2],scale_factor=0.02,scalars=sizes)
-        print len(sizes
+        
         mlab.show()
         
         for i,indices in enumerate(delaunay_grid.convex_hull):
