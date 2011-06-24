4c50074817cbb8ed5a2447ecefe0187f91fc2e94
Pieter Degroote <pieter.degroote@ster.kuleuven.be>
2011-06-24 10:00:55 +0200

    rochepotential.test

--- binary/rochepotential.py
+++ binary/rochepotential.py
@@ -1255,7 +1255,7 @@ def get_grid(*args,**kwargs):
         centers = np.zeros((len(grid.convex_hull),3))
         for i,indices in enumerate(grid.convex_hull):
             centers[i] = [x[indices].sum()/3,y[indices].sum()/3,z[indices].sum()/3]
-        theta,phi = np.arccos(centers[:,2]),arctan2(centers[:,1],centers[:,0])
+        theta,phi = np.arccos(centers[:,2]),np.arctan2(centers[:,1],centers[:,0])
         
         return theta.reshape((res1,res2)),phi.reshape((res1,res2)), grid
         
