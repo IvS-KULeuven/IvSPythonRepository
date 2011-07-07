4f651c3741de2df9ff068c1c6f4fda2d5b24b393
Pieter Degroote <pieter.degroote@ster.kuleuven.be>
2011-07-07 13:14:23 +0200

    test surface normal computation

--- asteroseismology/displacements.py
+++ asteroseismology/displacements.py
@@ -179,7 +179,7 @@ if __name__=="__main__":
     
                 for i,t in enumerate(np.linspace(0,2*pi,100)):
                     print k,l,m,i
-                    r,th,ph = surface(theta,phi,l,m,t,asl=asl,k=k)
+                    r,th,ph = surface(theta,phi,l,m,t,asl=0.,k=k)
                     if i==0: colors = r
                     x,y,z = vectors.spher2cart_coord(r,ph,th)
                     normal,size,cos_gamma = local.surface_normals(r,(theta,phi,grid),gtype='delaunay')
