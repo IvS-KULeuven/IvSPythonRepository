34c8783a8011cfa942e15ee43775f82c8946884b
Pieter Degroote <pieter.degroote@ster.kuleuven.be>
2011-07-07 16:34:15 +0200

    test surface normal computation

--- asteroseismology/displacements.py
+++ asteroseismology/displacements.py
@@ -180,20 +180,29 @@ if __name__=="__main__":
                 for i,t in enumerate(np.linspace(0,2*pi,100)):
                     print k,l,m,i
                     r,th,ph = surface(theta,phi,l,m,t,asl=asl,k=k)
-                    if i==0: colors = r
+                    
                     x,y,z = vectors.spher2cart_coord(r,ph,th)
                     center,size,normal = local.surface_normals(r,ph,th,gtype='triangular')
-                    mlab.figure(size=(1000,800))
-                    mlab.points3d(x,y,z,colors,scale_factor=0.05,scale_mode='none',colormap='RdBu',vmin=colors.min(),vmax=colors.max())
-                    #mlab.points3d(center.T[0],center.T[1],center.T[2],scale_factor=0.05,scale_mode='none')
+                    
+                    if i==0:
+                        colors = r
+                        colors_ = center
+                    
+                    
+                    mlab.clf()
+                    #mlab.points3d(x,y,z,colors,scale_factor=0.05,scale_mode='none',colormap='RdBu',vmin=colors.min(),vmax=colors.max())
+                    mlab.points3d(center.T[0],center.T[1],center.T[2],colors_,scale_factor=0.05,scale_mode='none',colormap='RdBu',vmin=colors_.min(),vmax=colors_.max())
                     #mlab.quiver3d(center.T[0],center.T[1],center.T[2],normal.T[0],normal.T[1],normal.T[2],colormap='spectral',scale_mode='none')
                     mlab.view(distance=5,azimuth=-90,elevation=90)
                     mlab.colorbar()
                     mlab.savefig('pulsation_lm%d%d_k%03d_%03d.png'%(l,m,k,i))
                     if i==1:
-                        mlab.quiver3d(center.T[0],center.T[1],center.T[2],center.T[0]-old_center.T[0],center.T[1]-old_center.T[1],center.T[2]-old_center.T[2],colormap='spectral')
+                        mlab.quiver3d(center.T[0],center.T[1],center.T[2],\
+                                      center.T[0]-old_center.T[0],\
+                                      center.T[1]-old_center.T[1],\
+                                      center.T[2]-old_center.T[2],colormap='spectral',scale_mode='none')
                         mlab.show()
-                    old_center = center
+                    old_center = center.copy()
                 mlab.close()
                 multimedia.make_movie('pulsation_lm%d%d_k%03d_*.png'%(l,m,k),output='pulsation_lm%d%d_k%03d.avi'%(l,m,k))
                 
