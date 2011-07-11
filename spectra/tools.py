49ccc64da5e06681d13d58ba74cfd4dc0353d48f
Pieter Degroote <pieter.degroote@ster.kuleuven.be>
2011-07-11 15:36:02 +0200

    updated doc

--- spectra/tools.py
+++ spectra/tools.py
@@ -19,7 +19,7 @@ resembling a main sequence star of spectral type A0. For this purpose, we do
 not need the whole spectrum but just cut out a piece
 
 >>> from ivs.spectra import model
->>> wave = np.linspace(4570,4574,200)
+>>> wave = np.linspace(4570,4574,300)
 >>> wave,flux,cont = model.get_table(teff=10000,logg=4.0,wave=wave)
 >>> clam = wave[np.argmin(flux)]
 
