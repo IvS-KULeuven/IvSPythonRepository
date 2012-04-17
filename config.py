a3c7a9c514b64b3ef5bcb4a47fa593c9a9e6f9e2
Joris Vos <jorisv@ster.kuleuven.be>
2012-04-17 15:52:35 +0200

    Added the option load_photometry to sed.builder.init()

--- config.py
+++ config.py
@@ -11,11 +11,7 @@ import glob as glob_module
 
 #-- You can add directories here, but be sure that the relative paths within
 #   those directories are correct!
-data_dirs = [os.getenv('ivsdata'),'/STER/pieterd/IVSDATA/', '/STER/kristofs/IVSdata']
-
-data_dirs = [os.getenv('ivsdata'),
-             '/STER/100/pieterd/IVSDATA/', 
-             '/STER/kristofs/IVSdata']
+data_dirs = [os.getenv('ivsdata'),'/STER/pieterd/IVSDATA/', '/STER/kristofs/IVSdata','/STER/jorisv/IVSDATA/']
              
 ivs_dirs = dict(coralie='/STER/coralie/',
                 hermes='/STER/mercator/hermes/')
