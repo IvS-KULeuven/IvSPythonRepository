1e4af2432894d99b5141d03d7fd2885ec053ba7d
Yannis Argyriou <ioannis.argyriou@ster.kuleuven.be>
2018-05-05 13:35:59 +0200

    Checked functionality of asteroseismology module and added IvS_repo_3.6_mac.yml script.

--- asteroseismology/displacements.py
+++ asteroseismology/displacements.py
@@ -3,12 +3,9 @@ Calculate the stellar surface displacements due to pulsations.
 
 
 """
-import numpy as np
-from numpy import sqrt,pi,sin,cos,exp
-from scipy.special import lpmv,legendre
-from scipy.misc.common import factorial
-from scipy.integrate import dblquad,quad
-from scipy.spatial import Delaunay
+from numpy import sqrt,pi,sin,cos,exp,log10,linspace,mgrid,polyval,zeros_like
+from scipy.special import legendre
+from scipy.misc import factorial
 
 #{ Helper functions
 
@@ -32,7 +29,7 @@ def legendre_(l,m,x):
     m_ = abs(m)
     legendre_poly = legendre(l)
     deriv_legpoly_ = legendre_poly.deriv(m=m_)
-    deriv_legpoly = np.polyval(deriv_legpoly_,x)
+    deriv_legpoly = polyval(deriv_legpoly_,x)
     P_l_m = (-1)**m_ * (1-x**2)**(m_/2.) * deriv_legpoly
     if m<0:
         P_l_m = (-1)**m_ * factorial(l-m_)/factorial(l+m_) * P_l_m
@@ -148,8 +145,8 @@ def surface(theta,phi,l,m,t,Omega=0.1,k=1.,asl=0.2,radius=1.):
         ksi_theta = asl*sqrt(4*pi)*colatitudinal(theta,phi,l,m,t,Omega,k)
         ksi_phi = asl*sqrt(4*pi)*longitudinal(theta,phi,l,m,t,Omega,k)
     else:
-        ksi_theta = np.zeros_like(theta)
-        ksi_phi = np.zeros_like(phi)
+        ksi_theta = zeros_like(theta)
+        ksi_phi = zeros_like(phi)
 
     return (radius+ksi_r.real),\
            (theta + ksi_theta.real),\
@@ -162,68 +159,147 @@ def observables(theta,phi,teff,logg,l,m,t,Omega=0.1,k=1.,asl=0.2,radius=1.,delta
         ksi_theta = asl*sqrt(4*pi)*colatitudinal(theta,phi,l,m,t,Omega,k)
         ksi_phi = asl*sqrt(4*pi)*longitudinal(theta,phi,l,m,t,Omega,k)
     else:
-        ksi_theta = np.zeros_like(theta)
-        ksi_phi = np.zeros_like(phi)
+        ksi_theta = zeros_like(theta)
+        ksi_phi = zeros_like(phi)
     gravity = 10**(logg-2)
     return (radius+ksi_r.real),\
            (theta + ksi_theta.real),\
            (phi + ksi_phi.real),\
            (teff + (delta_T*rad_part*teff).real),\
-           np.log10(gravity+(delta_g*rad_part*gravity).real)+2
+           log10(gravity+(delta_g*rad_part*gravity).real)+2
 
 #}
 
-if __name__=="__main__":
-    from ivs.roche import local
-    from ivs.coordinates import vectors
-    from enthought.mayavi import mlab
-    from divers import multimedia
-    theta,phi = local.get_grid(50,25,gtype='triangular')
-    r = np.ones_like(theta)
-    x,y,z = vectors.spher2cart_coord(r,phi,theta)
-    points = np.array([x,y,z]).T
-    grid = Delaunay(points)
-    #keep = phi>pi
-    #theta,phi = theta[keep],phi[keep]
-    l,m = 2,2
-    asl = 0.01
+def mainx():
+    # Test function: legendre_
+    ls,x = [0,1,2,3,4,5],cos(linspace(0,pi,100))
+    check = 0
+    for l in ls:
+        for m in range(-l,l+1,1):
+            Ppos = legendre_(l,m,x)
+            Pneg = legendre_(l,-m,x)
+            mycheck = Pneg,(-1)**m * factorial(l-m)/factorial(l+m) * Ppos
+            check += sum(abs(mycheck[0]-mycheck[1])>1e-10)
+    print(check) # should yield "0"
 
-    for k in [0,1.,2.]:
-        for l in range(1,5):
-            for m in range(0,l+1,1):
-                mlab.figure(size=(1000,800))
-                mlab.gcf().scene.disable_render = True
-                if l==0 or l==1:
-                    asl = 0.1
-                else:
-                    asl = 0.01
-                old_center=None
-                for i,t in enumerate(np.linspace(0,2*pi,100)):
-                    print(k,l,m,i)
-                    r,th,ph = surface(theta,phi,l,m,t,asl=asl,k=k)
-                    center,size,normal = local.surface_normals(r,ph,th,grid,gtype='triangular')
+    # Test function: sph_harm
+    import matplotlib.pyplot as plt
+    theta,phi = mgrid[0:pi:20j,0:2*pi:40j]
+    Ylm20 = sph_harm(theta,phi,2,0)
+    Ylm21 = sph_harm(theta,phi,2,1)
+    Ylm22 = sph_harm(theta,phi,2,2)
+    Ylm2_2 = sph_harm(theta,phi,2,-2)
 
-                    if i==0:
-                        colors = r
-                        r_c,phi_c,theta_c = vectors.cart2spher_coord(*center.T)
-                        colors_ = r_c
+    fig,axs = plt.subplots(1,4,figsize=(16,4))
+    fig.suptitle('Test of function <sph_harm>', fontsize=20)
+    axs[0].set_title('l=2,m=0');axs[0].imshow(Ylm20.real,cmap='jet')
+    axs[1].set_title('l=2,m=1');axs[1].imshow(Ylm21.real,cmap='jet')
+    axs[2].set_title('l=2,m=2');axs[2].imshow(Ylm22.real,cmap='jet')
+    axs[3].set_title('l=2,m=-2');axs[3].imshow(Ylm2_2.real,cmap='jet')
+    plt.tight_layout()
 
+    # Test functions: dsph_harm_dtheta, norm_J (called by dsph_harm_dtheta), and dsph_harm_dphi
+    spherical_harmonic_derivative_wrt_colatitude = dsph_harm_dtheta(theta,phi,l=2,m=1)
+    spherical_harmonic_derivative_wrt_longitude = dsph_harm_dphi(theta,phi,l=2,m=1)
 
-                    mlab.clf()
-                    mlab.points3d(center.T[0],center.T[1],center.T[2],colors_,scale_factor=0.05,scale_mode='none',colormap='RdBu',vmin=colors_.min(),vmax=colors_.max())
-                    #mlab.quiver3d(center.T[0],center.T[1],center.T[2],normal.T[0],normal.T[1],normal.T[2],colormap='spectral',scale_mode='none')
-                    mlab.colorbar()
+    fig,axs = plt.subplots(2,1,figsize=(12,8))
+    axs[0].set_title('Test of function <dsph_harm_dtheta>', fontsize=20)
+    axs[0].imshow(spherical_harmonic_derivative_wrt_colatitude.real,cmap='jet')
+    axs[1].set_title('Test of function <dsph_harm_dphi>', fontsize=20)
+    axs[1].imshow(spherical_harmonic_derivative_wrt_longitude.real,cmap='jet')
+    plt.tight_layout()
 
-                    if i>=1:
-                        vx,vy,vz = center.T[0]-old_center.T[0],\
-                                   center.T[1]-old_center.T[1],\
-                                   center.T[2]-old_center.T[2]
-                        v = np.sqrt(vx**2+vy**2+vz**2)
-                        mlab.quiver3d(center.T[0],center.T[1],center.T[2],\
-                                      vx,vy,vz,scalars=v,colormap='spectral',scale_mode='scalar')
-                        #mlab.show()
-                    old_center = center.copy()
-                    mlab.view(distance=5,azimuth=-90,elevation=90)
-                    mlab.savefig('pulsation_lm%d%d_k%03d_%03d.png'%(l,m,k,i))
-                mlab.close()
-                multimedia.make_movie('pulsation_lm%d%d_k%03d_*.png'%(l,m,k),output='pulsation_lm%d%d_k%03d.avi'%(l,m,k))
+    # Test functions: norm_atlp1,norm_atlm1
+    l,m = 2,1
+    Omega,k = 0.1,1
+    print(norm_atlp1(l,m,Omega,k))
+    print(norm_atlm1(l,m,Omega,k))
+
+    # Test function: radial
+    times = linspace(0,2*pi,100)
+    t = times[50]
+    radial_displacement = radial(theta,phi,l,m,t)
+
+    plt.figure()
+    plt.title('Test of function <radial>', fontsize=20)
+    plt.imshow(radial_displacement.real,cmap='jet')
+    plt.tight_layout()
+
+    # Test function: colatitudinal,longitudinal
+    fig,axs = plt.subplots(2,1,figsize=(12,8))
+    axs[0].set_title('Test of function <colatitudinal>', fontsize=20)
+    axs[0].imshow(colatitudinal(theta,phi,l,m,t,Omega,k).real,cmap='jet')
+    axs[1].set_title('Test of function <longitudinal>', fontsize=20)
+    axs[1].imshow(longitudinal(theta,phi,l,m,t,Omega,k).real,cmap='jet')
+    plt.tight_layout()
+    # plt.show()
+
+    # Test function: surface
+    test_surface = surface(theta,phi,l,m,t,Omega=0.1,k=1.,asl=0.2,radius=1.)
+    print(type(test_surface)) # returns a tuple
+
+    # Test function: observables
+    teff,logg = 5777,4.438 # approximate solar values
+    test_observables = observables(theta,phi,teff,logg,l,m,t,Omega=0.1,k=1.,asl=0.2,radius=1.,delta_T=0.01+0j,delta_g=0.01+0.5j)
+    print(type(test_observables)) # returns a tuple
+
+if __name__ == '__main__': mainx()
+
+#=========THE FOLLOWING EXAMPLE IS BROKEN (IMPORTED MODULES RAISE ERRORS) | USE AT YOUR OWN RISK==============
+
+# if __name__=="__main__":
+#     import numpy as np
+#     from ivs.roche import local
+#     from ivs.coordinates import vectors
+#     from enthought.mayavi import mlab
+#     from divers import multimedia
+#     from scipy.spatial import Delaunay
+#     theta,phi = local.get_grid(50,25,gtype='triangular')
+#     r = np.ones_like(theta)
+#     x,y,z = vectors.spher2cart_coord(r,phi,theta)
+#     points = np.array([x,y,z]).T
+#     grid = Delaunay(points)
+#     #keep = phi>pi
+#     #theta,phi = theta[keep],phi[keep]
+#     l,m = 2,2
+#     asl = 0.01
+#
+#     for k in [0,1.,2.]:
+#         for l in range(1,5):
+#             for m in range(0,l+1,1):
+#                 mlab.figure(size=(1000,800))
+#                 mlab.gcf().scene.disable_render = True
+#                 if l==0 or l==1:
+#                     asl = 0.1
+#                 else:
+#                     asl = 0.01
+#                 old_center=None
+#                 for i,t in enumerate(np.linspace(0,2*pi,100)):
+#                     print(k,l,m,i)
+#                     r,th,ph = surface(theta,phi,l,m,t,asl=asl,k=k)
+#                     center,size,normal = local.surface_normals(r,ph,th,grid,gtype='triangular')
+#
+#                     if i==0:
+#                         colors = r
+#                         r_c,phi_c,theta_c = vectors.cart2spher_coord(*center.T)
+#                         colors_ = r_c
+#
+#
+#                     mlab.clf()
+#                     mlab.points3d(center.T[0],center.T[1],center.T[2],colors_,scale_factor=0.05,scale_mode='none',colormap='RdBu',vmin=colors_.min(),vmax=colors_.max())
+#                     #mlab.quiver3d(center.T[0],center.T[1],center.T[2],normal.T[0],normal.T[1],normal.T[2],colormap='spectral',scale_mode='none')
+#                     mlab.colorbar()
+#
+#                     if i>=1:
+#                         vx,vy,vz = center.T[0]-old_center.T[0],\
+#                                    center.T[1]-old_center.T[1],\
+#                                    center.T[2]-old_center.T[2]
+#                         v = np.sqrt(vx**2+vy**2+vz**2)
+#                         mlab.quiver3d(center.T[0],center.T[1],center.T[2],\
+#                                       vx,vy,vz,scalars=v,colormap='spectral',scale_mode='scalar')
+#                         #mlab.show()
+#                     old_center = center.copy()
+#                     mlab.view(distance=5,azimuth=-90,elevation=90)
+#                     mlab.savefig('pulsation_lm%d%d_k%03d_%03d.png'%(l,m,k,i))
+#                 mlab.close()
+#                 multimedia.make_movie('pulsation_lm%d%d_k%03d_*.png'%(l,m,k),output='pulsation_lm%d%d_k%03d.avi'%(l,m,k))
--- asteroseismology/redgiantfreqs.py
+++ asteroseismology/redgiantfreqs.py
@@ -4,17 +4,17 @@ Functions for Red Giant Seismology.
 Contents:
 =========
 
-This library contains several functions which allow to identify modes 
-in the power-spectra of red giants. For the description of the pressure modes, 
-the formulism of Mosser et al. (2011A&A...525L...9M) for the 'Universal 
+This library contains several functions which allow to identify modes
+in the power-spectra of red giants. For the description of the pressure modes,
+the formulism of Mosser et al. (2011A&A...525L...9M) for the 'Universal
 Oscillation Pattern' is available.
 
-For the description of the frequencies and rotational splitting of the mixed 
+For the description of the frequencies and rotational splitting of the mixed
 dipole modes, the formalism of Mosser et al. (2012A&A...540A.143M) and (2012arXiv1209.3336M)
-has been scripted. To apply this method, one needs to know the value of the true 
-period spacing. The modulation of the mixing between pressure- and gravity-dipole 
-modes is described through a Lorentzian profile. This approach assumes a constant 
-Period Spacing for all radial orders. Comparing the modelled frequencies with the 
+has been scripted. To apply this method, one needs to know the value of the true
+period spacing. The modulation of the mixing between pressure- and gravity-dipole
+modes is described through a Lorentzian profile. This approach assumes a constant
+Period Spacing for all radial orders. Comparing the modelled frequencies with the
 real frequencies, found in an spectrum, will work well for the several radial
 orders but deviations can be found in other regions of the spectrum.
 A similar Lorentzian description is valid for rotational splitting.
@@ -22,21 +22,21 @@ A similar Lorentzian description is valid for rotational splitting.
 
 Contents:
 ---------
-    * purePressureMode: calculates the position of the pure pressure pressure 
+    * purePressureMode: calculates the position of the pure pressure pressure
     mode of a spherical degree \ell and radial order.
 
     * universalPattern: creates an array with the frequencies of all pure pressure
     modes with \ell=0,1,2 and 3, whereby the index i selectes all m
     odes of the same spherical degree:
-    e.g.: asymptoticRelation[0] = all \ell=0, asymptoticRelation[2] = all \ell=2, 
+    e.g.: asymptoticRelation[0] = all \ell=0, asymptoticRelation[2] = all \ell=2,
     asymptoticRelation[-1] gives an array with the radial orders (as float).
 
-    * asymptoticRelation: returns an array with the frequencies of the pure 
-    pressure modes of the pure pressure modes 
+    * asymptoticRelation: returns an array with the frequencies of the pure
+    pressure modes of the pure pressure modes
 
-    * rotational splitting: calculates the expected rotational splitting 
+    * rotational splitting: calculates the expected rotational splitting
     of a dipole mode based on the degree of mixing
-    
+
 
 Example: KIC 4448777
 --------------------
@@ -47,10 +47,10 @@ The powerspectrum can be found here: ...
 >>> centralRadial=  226.868   	# [muHz], Frequency of the central radial mode
 >>> largeSep	= 17.000		# [muHz], large Frequency separation in powerspectrum
 >>> smallSep	= 2.201			# [muHz], small Frequency separation in powerspectrum
->>> 
+>>>
 >>> DeltaPg		= 89.9			# [seconds],	true period spacing of dipole modes
 >>> dfMax		= 0.45			# [muHz], normalized rotational splitting (i.e. |freq_{m=0} - freq_{m=+/-1}|
->>> 
+>>>
 >>> qCoupling= 0.15; lambdaParam = 0.5; beParam = 0.08 #Default values
 
 First model the frequencies of the pure pressure modes.
@@ -92,7 +92,7 @@ logger = logging.getLogger('IVS.RG')
 def purePressureMode(nnn,centralRadial, largeSep, smallSep, epsilon, ell):
     """
     Calculates and returns the frequency of a single pure pressure mode for a spherical (\ell=0,1,2, and 3)[Nota Bene: 3 to be implemented].
-    
+
     smallSep can be set to an abitrary value if not calculating \ell=2 modes.
 
 	@param nnn: radial order of the mode with respect to the radial order of the central radial mode.
@@ -102,7 +102,7 @@ def purePressureMode(nnn,centralRadial, largeSep, smallSep, epsilon, ell):
 	@param smallSep: small Frequency separation in powerspectrum, in muHz
 	@type smallSep: float
 	@param centralRadial: Frequency of the central radial mode, in muHz
-	@type centralRadial: float	
+	@type centralRadial: float
 	@param epsilon: echelle phase shift of the central Radial mode:  epsilon = centralRadial / largeSep - centralRadial // largeSep
 	@type epsilon: float
 	@param ell:  spherical degree of a mode
@@ -113,7 +113,7 @@ def purePressureMode(nnn,centralRadial, largeSep, smallSep, epsilon, ell):
 
     ell = np.float(ell)
     nnn = np.float(nnn)
-    smallSep /= largeSep 
+    smallSep /= largeSep
     dominantRadialOrderPressure = np.floor(centralRadial / largeSep)
     alphaCorr = 0.015 * largeSep**(-0.32)
 
@@ -125,7 +125,7 @@ def purePressureMode(nnn,centralRadial, largeSep, smallSep, epsilon, ell):
     purePressureModeEll = largeSep * ( (dominantRadialOrderPressure + nnn) + epsilon - constantDEll + alphaCorr/2.*(nnn)**2.)
 
     return purePressureModeEll
-    
+
 
 
 
@@ -133,11 +133,11 @@ def purePressureMode(nnn,centralRadial, largeSep, smallSep, epsilon, ell):
 
 def universalPattern(largeSep, smallSep, centralRadial, numberOfRadialOrders = 3):
 
-    """ 
-    Calculates and returns an array containing frequencies the frequencies of the pure pressure modes 
-    of the degrees spherical degrees \ell= 0,1,2 and 3 for a defined range of radial orders.  
-    
-    When slicing, the index number is equal to the spherical degree \ell, universalPattern[0] = all \ell=0, universalPattern[2] = all \ell=2, 
+    """
+    Calculates and returns an array containing frequencies the frequencies of the pure pressure modes
+    of the degrees spherical degrees \ell= 0,1,2 and 3 for a defined range of radial orders.
+
+    When slicing, the index number is equal to the spherical degree \ell, universalPattern[0] = all \ell=0, universalPattern[2] = all \ell=2,
     The number of the radial order is given as output[-1]. If desired, the array can also be printed to the screen.
 
 	@param largeSep: large Frequency separation in powerspectrum, in muHz
@@ -145,39 +145,39 @@ def universalPattern(largeSep, smallSep, centralRadial, numberOfRadialOrders = 3
 	@param smallSep: small Frequency separation in powerspectrum, in muHz
 	@type smallSep: float
 	@param centralRadial: Frequency of the central radial mode, in muHz
-	@type centralRadial: float	
+	@type centralRadial: float
 	@param numberOfRadialOrders: for how many radial orders above and below the Central Radial Mode should the frequencies be calculated.
 	@type numberOfRadialOrders: float or integer
 
 	@return array with frequencies in muHz, [-1] indicates the radial order.
 	@rtype float array
     """
-    
-   
+
+
     epsilon = centralRadial / largeSep - centralRadial // largeSep
     radialOrderRange = np.arange(-numberOfRadialOrders,numberOfRadialOrders+1,dtype='float')
     dominantRadialOrderPressure = np.floor(centralRadial/largeSep)
     logger.info('dominantRadialOrderPressure {0}'.format(dominantRadialOrderPressure))
     univPatternPerOrder = []
-    
+
     for nnn in radialOrderRange:
         radial =     purePressureMode(nnn,centralRadial, largeSep, smallSep, epsilon, 0.)
         dipole =     purePressureMode(nnn,centralRadial, largeSep, smallSep, epsilon, 1.)
         quadrupole = purePressureMode(nnn,centralRadial, largeSep, smallSep, epsilon, 2.)
         septupole =  purePressureMode(nnn,centralRadial, largeSep, smallSep, epsilon, 3.)
-        
+
         univPatternPerOrder.append([radial,dipole,quadrupole,septupole,nnn+dominantRadialOrderPressure])
-    
+
     univPatternPerOrder = np.array(univPatternPerOrder, dtype='float')
-    
- 
+
+
     logger.info('radial\t\tdipole\t\tquadrupole\tseptupole\tRadial Order (Pressure)')
     logger.info('--------------------------------------------------------------------------------------')
     for modeValue in univPatternPerOrder: logger.info('{0:.3f}\t\t{1:.3f}\t\t{2:.3f}\t\t{3:.3f}\t\t{4}'.format(modeValue[0],modeValue[1],modeValue[2],modeValue[3],modeValue[4]))
-    logger.info('--------------------------------------------------------------------------------------')  
+    logger.info('--------------------------------------------------------------------------------------')
 
     return univPatternPerOrder.T
-    
+
 
 
 
@@ -185,11 +185,11 @@ def universalPattern(largeSep, smallSep, centralRadial, numberOfRadialOrders = 3
 def asymptoticRelation(centralRadial, largeSep, DeltaPg, qCoupling= 0.15,
                        approximationTreshold=0.001, numberOfRadialOrders = 4):
     """ Calculates and returns the frequencies mixed dipole modes in the powerspectrum of a red-giant star.
-    
+
     This function the modulation of the mixing between pressure- and gravity-dipole and derives the frequencies of the mixed modes from it.
 
 	@param centralRadial: Frequency of the central radial mode, in muHz
-	@type centralRadial: float	
+	@type centralRadial: float
 	@param largeSep: large Frequency separation in powerspectrum, in muHz
 	@type largeSep: float
 	@param smallSep: small Frequency separation in powerspectrum, in muHz
@@ -198,15 +198,15 @@ def asymptoticRelation(centralRadial, largeSep, DeltaPg, qCoupling= 0.15,
 	@type DeltaPg: float
 	@param qCoupling: coupling factor
 	@type qCoupling: float
-	
+
 	@param approximationTreshold: prefiltering of the solution. if modes seem to be missing, increase this parameter
 	@type approximationTreshold:float
 	@param numberOfRadialOrders: for how many radial orders above and below the Central Radial Mode should the frequencies be calculated.
-	@type numberOfRadialOrders: float or integer	
+	@type numberOfRadialOrders: float or integer
 	@return frequencies of the m=0 component of the mixed dipole modes
 	@rtype float array
     """
-    
+
     DeltaPg /= 10.**6.
     epsilon = centralRadial / largeSep - centralRadial // largeSep
     radialOrderRange = np.arange(-numberOfRadialOrders,numberOfRadialOrders+1.,dtype='float')
@@ -220,15 +220,15 @@ def asymptoticRelation(centralRadial, largeSep, DeltaPg, qCoupling= 0.15,
         valueDipole =  purePressureMode(nnn,centralRadial, largeSep, 2., epsilon, 1.)
         freqRange =    np.linspace((centralRadial+largeSep*(nnn-0.25)),  (centralRadial+largeSep*(nnn+1.25)), 100000)
         equalityLine = freqRange/largeSep
-        arcTerm = largeSep / scipy.pi * np.arctan( qCoupling * np.tan( scipy.pi / (DeltaPg*freqRange) ) )        
+        arcTerm = largeSep / scipy.pi * np.arctan( qCoupling * np.tan( scipy.pi / (DeltaPg*freqRange) ) )
         #print 'nnn',nnn,', pure pressure dipole',valueDipole,min(freqRange),max(freqRange)
-        
+
         # distance to equalityLine
         residualsToEquealityLine = np.array((freqRange/largeSep,(valueDipole+arcTerm)/largeSep-equalityLine)) # Frequenz Abstand der moeglichen Mixed Modes zur _equalityLine_
 
         # filtering for the close modes, speeds up the process
         filteredResidualsToEquealityLine = residualsToEquealityLine[:,(residualsToEquealityLine[1] < approximationTreshold/1.)]    # nur die, die nahe genug an der Mixed Mode liegen
-        
+
         """ # plotting the method to solve the implicit formula
         pl.figure(num=2)
         pl.plot(freqRange/largeSep,equalityLine,'r', alpha=0.5)
@@ -256,7 +256,7 @@ def asymptoticRelation(centralRadial, largeSep, DeltaPg, qCoupling= 0.15,
 
 def symmetricRotationalModulation(dfMax, largeSep, frequenciesUniversalPattern, mixedModesPattern, lambdaParam = 0.5, beParam = 0.08):
     """ Calculates and returns rotational splitting
-    
+
     This function the modulation of the mixing between pressure- and gravity-dipole and derives the frequencies of the mixed modes from it.
 
 	@param dfMax: the largest rotational splitting measured for a given star
@@ -275,31 +275,48 @@ def symmetricRotationalModulation(dfMax, largeSep, frequenciesUniversalPattern, 
 
 	@param beParam:  parameter for fitting, Default: beParam = 0.08
 	@type beParam: float
-	
-	@return array with the rotational splitting 
+
+	@return array with the rotational splitting
 	@rtype array, float
     """
 
 
-    
+
     radialModes =         frequenciesUniversalPattern[0]
     pureDipolePressure =    frequenciesUniversalPattern[1]
     mixedDipoleMode =        mixedModesPattern
-    
+
     rotationalCentreMode = []; rotationalSplitting = []
     for nnn in np.arange(0,len(radialModes.T)-1):
         pureDipole =    pureDipolePressure[ (pureDipolePressure > radialModes[nnn]) & (pureDipolePressure < radialModes[nnn+1])]
-        mixedDipoles=    mixedDipoleMode[ (mixedDipoleMode > radialModes[nnn]) & (mixedDipoleMode < radialModes[nnn+1])] 
+        mixedDipoles=    mixedDipoleMode[ (mixedDipoleMode > radialModes[nnn]) & (mixedDipoleMode < radialModes[nnn+1])]
 
         for mode in mixedDipoles:
             modulationProfile = 1.- lambdaParam / (1. + ( (mode - pureDipole[0]) / (beParam*largeSep) )**2. )
-            rotationalCentreMode.append(mode) 
+            rotationalCentreMode.append(mode)
             rotationalSplitting.append(modulationProfile)# i.e., frequency of m=0 component, symmetric rotational splitting df. m=0 +/- df
 
     rotationalCentreMode = np.array(rotationalCentreMode)
     rotationalSplitting = np.array(rotationalSplitting).T*dfMax
     #print shape(rotationalCentreMode),shape(rotationalSplitting)
-    rotationalSplitting = np.vstack((rotationalCentreMode,rotationalSplitting))  
+    rotationalSplitting = np.vstack((rotationalCentreMode,rotationalSplitting))
     rotationalSplitting = np.array(rotationalSplitting, dtype='float')
-    
+
     return rotationalSplitting
+
+def mainx():
+    centralRadial=  226.868   	# [muHz], Frequency of the central radial mode
+    largeSep	= 17.000		# [muHz], large Frequency separation in powerspectrum
+    smallSep	= 2.201			# [muHz], small Frequency separation in powerspectrum
+
+    DeltaPg		= 89.9			# [seconds],	true period spacing of dipole modes
+    dfMax		= 0.45			# [muHz], normalized rotational splitting (i.e. |freq_{m=0} - freq_{m=+/-1}|
+    qCoupling= 0.15; lambdaParam = 0.5; beParam = 0.08 #Default values
+
+    # First model the frequencies of the pure pressure modes.
+    frequenciesUniversalPattern = universalPattern(largeSep, smallSep, centralRadial)
+    # Next, we calculate the frequencies of the mixed dipole modes:
+    mixedModesPattern = asymptoticRelation(centralRadial,largeSep, DeltaPg, qCoupling)
+    rotationalSplittingModulation = symmetricRotationalModulation(dfMax, largeSep, frequenciesUniversalPattern, mixedModesPattern, lambdaParam, beParam)
+
+if __name__ == '__main__': mainx()
